5d6474d9b17981c796fcff4652ffff98c9b193ae
theMungai <mungaithesimon@gmail.com>
2024-09-17 09:38:34 +0300

    JavaScript Basic String methods

---
+++ js/main.js
@@ -0,0 +1,39 @@
+console.log("Hello World");
+
+// Strings Methods 
+
+// 1. Length property
+const myVariable = "Mathematics";
+console.log(myVariable.length);
+
+// 2. Character at method
+const myName = "Simon Mungai";
+console.log(myName.charAt(4));
+
+
+// 3. Indexof method
+const bootCamp = "PowerLearn"
+console.log(bootCamp.indexOf("ear"));
+
+// 4. Slice Method
+const myLanguage = "JavaScript";
+console.log(myLanguage.slice(3,8));
+
+// 5. toUpperCase method
+const secondName = "mungai";
+console.log(secondName.toUpperCase());
+
+// 6 . toLowerCase method
+const myPage = "DARKMODE";
+console.log(myPage.toLowerCase());
+
+/* 7. the 'includes' method returns Boolean data. 
+    - If the character we provide are not in the string it returns 'False'
+*/
+
+const mySentence = "JavaScript  is awesome";
+console.log(mySentence.includes("Jav"));
+
+// 8. Split method
+const myProject = "JavaScript Calculator";
+console.log(myProject.split("ava"))
